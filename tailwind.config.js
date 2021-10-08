54788d6958c50a8da99ae7e0540c157bd9fd7562
Drew-Js <mulumbaandrew1@gmail.com>
2021-10-08 16:07:41 +0530

    Landing-Page

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    extend: {},
+    fontfamily:{
+      Roboto:["Roboto, sans-serif"],
+
+    },
+    container :{
+      center: true,
+      padding: "1rem",
+      screens :{
+        lg:"1124px",
+        xl:"1124px",
+        "2xl":"1124px",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
